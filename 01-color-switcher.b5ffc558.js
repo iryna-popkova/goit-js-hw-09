0e5d8e3017a28e7f445acaa9d42cd871eff7f656
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-09-15 09:39:04 +0000

    Deploying to gh-pages from @ iryna-popkova/goit-js-hw-09@5cb163bb4987a58da2f3a1c5185d36653a8c4f64 🚀

---
+++ 01-color-switcher.b5ffc558.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(e){n=setInterval(o,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(e){clearInterval(n),t.removeAttribute("disabled")}));var n=null;function o(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}}();
+//# sourceMappingURL=01-color-switcher.b5ffc558.js.map
---
+++ 01-color-switcher.bac0db90.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.querySelector("body");t.addEventListener("click",(function(e){r=setInterval(n,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(e){clearInterval(r),t.removeAttribute("disabled")}));let r=null;function n(){o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}
+//# sourceMappingURL=01-color-switcher.bac0db90.js.map
