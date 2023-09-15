5cb163bb4987a58da2f3a1c5185d36653a8c4f64
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-09-15 12:38:39 +0300

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+startButton.addEventListener('click', onStartButton);
+stopButton.addEventListener('click', onStopButton);
+
+let timerId = null;
+
+function onStartButton(event) {
+  timerId = setInterval(colorChange, 1000);
+  startButton.toggleAttribute('disabled')
+};
+
+function onStopButton(event) {
+  clearInterval(timerId);
+
+  startButton.removeAttribute('disabled')
+}
+
+
+
+function colorChange() {
+  body.style.backgroundColor = getRandomHexColor();
+};
+
---
+++ src/js/02-timer.js
@@ -0,0 +1,11 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+require("flatpickr/dist/themes/dark.css");
+
+import Notiflix from 'notiflix';
+
+
+const startBtn = document.querySelector('[data-start]');
+
