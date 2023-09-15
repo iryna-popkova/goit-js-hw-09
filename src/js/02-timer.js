fce865e5854acbfb17239688bc25e10a8b82f180
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-09-15 18:02:20 +0300

    second task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,11 +1,62 @@
-// Описаний в документації
+
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 require("flatpickr/dist/themes/dark.css");
-
 import Notiflix from 'notiflix';
 
 
 const startBtn = document.querySelector('[data-start]');
+const dataInput = document.querySelector('#datetime-picker');
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
 
+startBtn.setAttribute('disabled','true');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    if (selectedDates[0] <= new Date()) {
+      window.alert("Please choose a date in the future");
+startBtn.setAttribute('disabled','true');
+    } else startBtn.removeAttribute('disabled');
+  }
+}
+
+flatpickr("#datetime-picker", options);
+
+startBtn.addEventListener('click', onStartBtn);
+
+let timerId = null;
+
+function onStartBtn() {
+  timerId = setInterval(() => {
+
+  }, 1000);
+}
+
+
+
+function convertMs(ms) {
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000));
+console.log(convertMs(140000));
+console.log(convertMs(24140000));
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,17 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmition);
+
+function onFormSubmition(event) {
+  event.preventDefault();
+  let delay = Number(form.delay.value);
+
+
+};
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
