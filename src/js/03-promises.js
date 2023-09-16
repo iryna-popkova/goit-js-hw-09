c74b0e1d9890c811a68e78e5c20cadca56dae461
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-09-16 16:51:43 +0300

    third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,17 +1,5 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const form = document.querySelector('.form');
-form.addEventListener('submit', onFormSubmition);
-
-function onFormSubmition(event) {
-  event.preventDefault();
-  let delay = Number(form.delay.value);
-
-
-};
-
-
-
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -20,3 +8,20 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmition);
+
+function onFormSubmition(event) {
+  event.preventDefault();
+
+  let firstDelay = Number(form.delay.value);
+  let delayStep = Number(form.step.value);
+  for (let i = 1; i < form.amount.value; i += 1) {
+    const element = form.amount[i];
+    let delay = firstDelay + delayStep * (i - 1);
+  } return
+
+  }
+
+
